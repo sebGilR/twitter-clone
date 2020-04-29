cbc782ce107076291c06f5e928957e91d32a4011
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-04-29 10:51:11 -0500

    Add views for sign up, log-in and password recovery

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < Devise::RegistrationsController
+    def new
+        @user = User.new
+    end
+
+    private
+    def sign_up_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+    end
+end
